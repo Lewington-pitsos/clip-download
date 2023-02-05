10b56292c9beddb6e2635366f0bdf3301e17fbdc
Steffen L. Norgren <steffen@norgren.ca>
2023-02-05 01:53:44 -0800

    Rewrite
    json file passed as argument
    download location passed as argument
    
    downloads in 8 parallel streams

--- download.py
+++ download.py
@@ -1,42 +1,49 @@
-import json
-import requests 
 import os
-from PIL import Image
+import sys
+import json
+import concurrent.futures
+import requests
 
-input_file = "clipsubset.json"
+def download_file(url, save_path):
+    # Get the file name from the URL
+    file_name = url.split("/")[-1]
+    save_location = os.path.join(save_path, file_name)
 
-with open(input_file, encoding="utf8") as f:
-    data = json.load(f)
+    # Download the file
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get("Content-Length", 0))
+    progress = 0
+    with open(save_location, "wb") as f:
+        for data in response.iter_content(1024):
+            progress += len(data)
+            f.write(data)
+            done = int(50 * progress / total_size)
+            print(f"\r[{'=' * done}>{' ' * (50-done)}] {progress}/{total_size}", end="")
+    print(f"\nDownloaded {file_name}")
 
-count = 0
-testpath = "tst.jpg"
+def main(json_file, save_path):
+    # Load the JSON file into memory
+    with open(json_file) as file:
+        data = json.load(file)
 
-for row in data:
-    image_url = row['url']
-    try:
+    # Extract the URLs from the JSON data
+    urls = [item["url"] for item in data]
 
-        response = requests.get(image_url, stream = True)
+    # Download the files in parallel
+    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+        futures = [executor.submit(download_file, url, save_path) for url in urls]
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
 
-        if response.status_code == 200:
-            try:
-                img = Image.open(response.raw)
-                path = f'{count}.{img.format}'
-                img.save(os.path.join(testpath))
+if __name__ == "__main__":
+    # Check if the name of the JSON file and the download location were passed as arguments
+    if len(sys.argv) < 3:
+        print("Error: missing arguments for JSON file name and download location")
+        sys.exit(1)
 
-                img = Image.open(testpath)
-                img.verify()
+    # Get the name of the JSON file and the download location from the command-line arguments
+    json_file = sys.argv[1]
+    save_path = sys.argv[2]
 
-                img = Image.open(testpath)
-                img.save(os.path.join('hd', path))
-                os.remove(testpath)
-
-                print('Image sucessfully Downloaded')
-                count+=1
-            except Exception as e:
-                if "seek" in str(e):
-                    raise(e)
-                print("encountered exception when reading image data from url", e)              
-        else:
-            print('Image Couldn\'t be retreived')
-    except Exception as e:
-        print("encountered connection error", e) 
+    # Call the main function
+    main(json_file, save_path)
