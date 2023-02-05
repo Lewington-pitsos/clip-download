8fffca0038759ad03b8a0333c55f2902a7ebcadc
Steffen L. Norgren <steffen@norgren.ca>
2023-02-05 03:21:33 -0800

    More sanity

--- download.py
+++ download.py
@@ -6,10 +6,18 @@ import json
 import concurrent.futures
 import requests
 
-def download_file(url, save_path):
+def download_file(item, save_path):
     # Get the file name from the URL
+    url = item["url"]
     file_name = url.split("/")[-1]
-    save_location = os.path.join(save_path, file_name)
+    id = item["id"]
+    extension = file_name.split(".")[-1]
+    save_location = os.path.join(save_path, f"{id}.{extension}")
+
+    # Check if the file is a png or jpg
+    if extension not in ["png", "PNG", "jpg", "JPG", "jpeg", "JPEG"]:
+        print(f"\nSkipping {id}.{extension}")
+        return
 
     # Download the file
     response = requests.get(url, stream=True)
@@ -19,25 +27,20 @@ def download_file(url, save_path):
         for data in response.iter_content(1024):
             progress += len(data)
             f.write(data)
-            done = int(50 * progress / total_size)
-            print(f"{file_name}: [{'=' * done}>{' ' * (50-done)}] {progress}/{total_size}", end="")
-    print(f"\nDownloaded {file_name}")
+    print(f"\nDownloaded {id}.{extension}")
+
 
 def main(json_file, save_path, max_workers):
     # Load the JSON file into memory
     with open(json_file) as file:
         data = json.load(file)
 
-    # Extract the URLs from the JSON data
-    urls = [item["url"] for item in data]
-
     # Create the download directory if it doesn't exist
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+    os.makedirs(save_path, exist_ok=True)
 
     # Download the files in parallel
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = [executor.submit(download_file, url, save_path) for url in urls]
+        futures = [executor.submit(download_file, item, save_path) for item in data]
         for future in concurrent.futures.as_completed(futures):
             future.result()
 
@@ -46,19 +49,14 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Error: missing argument for JSON file name")
         sys.exit(1)
+    elif len(sys.argv) < 3:
+        print("Error: missing argument for download location")
+        sys.exit(1)
 
-    # Get the name of the JSON file from the command-line arguments
+    # Get the name of the JSON file and the download location from the command-line arguments
     json_file = sys.argv[1]
-    
-    # Set the download location to the same name as the json file, but without the extension
-    save_path = os.path.splitext(json_file)[0]
-    if len(sys.argv) >= 3:
-        save_path = sys.argv[2]
-    
-    # Set the number of concurrent downloads
-    max_workers = 8
-    if len(sys.argv) >= 4:
-        max_workers = int(sys.argv[3])
+    save_path = os.path.splitext(json_file)[0] if len(sys.argv) < 3 else sys.argv[2]
+    max_workers = 8 if len(sys.argv) < 4 else int(sys.argv[3])
 
     # Call the main function
     main(json_file, save_path, max_workers)
